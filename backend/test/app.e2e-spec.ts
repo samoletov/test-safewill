e5ac4e1e2de988b6d23b001324f4f02a7d8d689a
Johnny Samoletov <johnny_samoletov@amp.com.au>
2021-02-18 18:11:31 +1100

    chore: e2e tests

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,22 +1,80 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from 'src/movie.entity';
+import { IMovie } from 'src/movie.interface';
 import * as request from 'supertest';
+import { Repository } from 'typeorm';
 
 import { MoviesModule } from '../src/movies.module';
+import * as initData from './data/init.json';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
+  let repository: Repository<Movie>;
+  let entities: Movie[];
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [MoviesModule],
+      imports: [
+        MoviesModule,
+        TypeOrmModule.forRoot({
+          type: 'mongodb',
+          host: 'localhost',
+          port: 27017,
+          username: 'root',
+          password: 'example',
+          database: 'movies',
+          entities: ['**/*.entity.ts'],
+          synchronize: true,
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    repository = moduleFixture.get('MovieRepository');
   });
 
-  it('/list (GET)', () => {
-    return request(app.getHttpServer()).get('/list').expect(200);
+  afterAll(async () => {
+    await app.close();
+  });
+
+  beforeEach(async () => {
+    entities = await repository.save(
+      initData.map((item: IMovie) => {
+        const movie = new Movie();
+        movie.title = item.title;
+        movie.rating = item.rating;
+        movie.logoUrl = item.logoUrl;
+        movie.description = item.description;
+        return movie;
+      }),
+    );
+  });
+
+  afterEach(async () => {
+    await repository.clear();
+  });
+
+  it('/list (GET)', async () => {
+    const { status, body } = await request(app.getHttpServer()).get('/list');
+    expect(status).toEqual(200);
+    expect(body).toHaveLength(10);
+    expect(body[0]).toHaveProperty('id');
+    expect(body[0]).toHaveProperty('title');
+    expect(body[0].title).toEqual('Fight Club');
+    expect(body[0].description).toBeUndefined();
+    expect(body[0].logoUrl).toBeUndefined();
+    expect(body[0].rating).toBeUndefined();
+  });
+
+  it('/item/{id} (GET)', async () => {
+    const { status, body } = await request(app.getHttpServer()).get(`/item/${entities[0].id}`);
+    expect(status).toEqual(200);
+    expect(body).toHaveProperty('title');
+    expect(body).toHaveProperty('id');
+    expect(body).toHaveProperty('description');
+    expect(body).toHaveProperty('logoUrl');
+    expect(body).toHaveProperty('rating');
   });
 });
